e3c97e7f2fe2fb7006a0d71715db7805cd485cc8
deepaksinhgh9958 <deepaksingh995834@gmail.com>
2022-03-07 05:40:51 +0530

    First Commit

---
+++ practice project assisment/src/q2_AccessModifirs/Data.java
@@ -0,0 +1,14 @@
+package q2_AccessModifirs;
+
+class Data {
+    private String name;
+
+    // getter method
+    public String getName() {
+        return this.name;
+    }
+    // setter method
+    public void setName(String name) {
+        this.name= name;
+    }
+}
---
+++ practice project assisment/src/q2_AccessModifirs/DefaultAccess.java
@@ -0,0 +1,11 @@
+package q2_AccessModifirs;
+
+public class DefaultAccess {
+	static void Logger(){
+        System.out.println("This is default");
+    }
+	public static void main (String [] args) {
+		
+		Logger();
+	}
+}
